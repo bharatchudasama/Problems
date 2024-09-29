314c6761f1bb219c1cf54c77eb17d1f212e19264
Bharat chudasama <bharatchudasama056@gmail.com>
2024-09-29 15:40:19 +0530

    minor error

--- script/approaches.js
+++ script/approaches.js
@@ -22,10 +22,10 @@ const approachesData = {
   8: [
     { id: 1, name: "Approach 1", file: "p1.cpp" }
   ],
-9: [
+  9: [
     { id: 1, name: "Approach 1", file: "p1.cpp" },
     { id: 2, name: "Approach 2", file: "p2.cpp" },
-    { id: 3, name: "Approach 3", file: "p3.cpp" }
+    { id: 3, name: "Approach 3", file: "p3.cpp" },
     { id: 4, name: "Approach 4", file: "p4.cpp" }
   ],
   // Add other problem IDs and their approaches here
